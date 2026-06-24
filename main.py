acc4bc0db7bfc5096ba9648675db756187b0ccc6
scary321 <139805188+scary321@users.noreply.github.com>
2026-06-25 02:06:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pandas
 import random
 import smtplib
 
-EMAIL = os.environ.get("MY_EMAIL")
-PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
@@ -21,7 +21,7 @@ if today_tuple in birthdays_dict:
 
     with smtplib.SMTP("smtp.gmail.com",587) as connection:
         connection.starttls()
-        connection.login(EMAIL, PASSWORD)
+        connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
             to_addrs=birthday_person["email"],
